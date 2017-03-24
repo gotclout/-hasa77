ef081fb903edab345d20ad2c2d523787d928e5a1
gotclout <rlfosterjr@gmail.com>
2017-03-24 19:29:47 -0400

    fixed.

--- compress.pl
+++ compress.pl
@@ -1,7 +1,14 @@
-compress(Xs, Z) :- length(Xs, XsL), Sz is mod(XsL, 2), N is div(XsL,2) + Sz,
-                   Sz > 0 -> compressed(odd, Xs, N, Y, Z) ; compressed(even, Xs, N, Y, Z).
+% 1.17 (*): Split a list into two parts
 
-compressed(even, Xs, N, Y, Z) :- compressed(Xs, N, Y, Z).
-compressed(odd, Xs, N, Y, Z) :- compressed(Xs, N, Y, Z).
-compressed(L,0,[],L).
-compressed([X|Xs],N,[X|Ys],Z) :-  N > 0, N1 is N - 1, compressed(Xs, N1, Ys, Z).
+% split(L,N,L1,L2) :- the list L1 contains the first N elements
+%    of the list L, the list L2 contains the remaining elements.
+%    (list,integer,list,list) (?,+,?,?)
+
+split(Xs, N, Z) :- split(Xs, N, Z, _).
+compress(Xs, Z) :- length(Xs, XsL), Sz is mod(XsL, 2), N is div(XsL,2) + Sz, Sz > 0 -> split(Xs, N, Z, _), write('compressed(odd,'), write(Z), write(')') ; split(Xs, N, Z, _), write('compressed(even'), write(Z), write(')').
+compress( L,0,[],L).
+compressed(odd, Z).
+compressed(even, Z).
+split(L,0,[],L).
+split([X|Xs],N,[X|Ys],Zs) :- N > 0, N1 is N - 1, split(Xs,N1,Ys,Zs).
+
